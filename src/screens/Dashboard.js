3ca13ae41e3fec822fa4d4b5b6ece71ce2d48ae2
Alex Milyukov <amilyukov@apple.com>
2019-03-10 13:49:51 -0700

    Adding a dummy chart

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom';
 import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography'
+import SimpleLineChart from './../containers/SimpleLineChart';
 
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  tableContainer: {
+    height: 320,
+  }
+});
 
 class Dashboard extends Component {
 
   render() {
+
+    const { classes } = this.props;
+
     return (
-      <div>
-        Dashboard stuff
+      <div className={classes.root}>
+        <Typography variant="h4" gutterBottom component="h2">
+            Executions
+        </Typography>
+        <div className={classes.tableContainer}>
+          <SimpleLineChart />
+        </div>
       </div>
     );
   }
 }
 
-export default Dashboard;
+export default withStyles(styles)(Dashboard);
